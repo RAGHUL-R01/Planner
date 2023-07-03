18073e703d40fe6edf1230fcc24359f011948ee5
RAGHUL-R01 <raghulnishanth195@gmail.com>
2023-07-03 13:24:43 +0530

    Added Done items

--- Script.js
+++ Script.js
@@ -5,17 +5,21 @@ let Yesterday_old_data = []
 function CreateElements(Array1, parentclassname, childclassname) {
     for (let a = 0; a < Array1?.length; a++) {
         let items_ = document.getElementById(parentclassname);//Reference div to create element
-        let Element = document.createElement('div');//creating parent div for todo and btns
+        let Element = document.createElement('li');//creating parent div for todo and btns
         Element.setAttribute("class", childclassname);//class name for parent div
         items_.append(Element)//make the parent div as child to another div
 
         //Creating content div and add the item from Array
         let Todo_item = document.createElement('div');
-        Todo_item.setAttribute("class", "TodoContent");
-        Todo_item.innerText = Array1[a];
-        Element.append(Todo_item);
-
+        if (childclassname === 'doneitmslist5') {
+            Element.innerText = Array1[a];
+            items_.append(Element)
+            // Todo_item = document.createElement('li')
+        }
         if (childclassname === "TodoItems") {
+            Todo_item.setAttribute("class", "TodoContent");
+            Todo_item.innerText = Array1[a];
+            Element.append(Todo_item);
             //Add the button to remove the item from array
             let Btn_Done = document.createElement('button')
             Btn_Done.innerText = 'DONE'
@@ -32,10 +36,18 @@ function ClearLocalstorageItem(ItemsToRemove) {
     Temp_yesterday = Temp_yesterday.filter(element => element !== ItemsToRemove);
     // console.log(Temp_yesterday)
 }
-
+// to show the done
+function ShowDone() {
+    let Temp_yesterday = window.localStorage.getItem("DONE") || null
+    Temp_yesterday = Temp_yesterday?.split(',');
+    CreateElements(Temp_yesterday, "doneItems", "doneitmslist5");
+    // console.log(Temp_yesterday)
+}
 const AddItems = function () {
+    // debugger;
     let input_value = document.getElementById("Yesterday").value;
-    if (input_value !== '') {
+    console.log(input_value)
+    if (input_value !== "" && input_value !== undefined) {
         Yesterday.push(input_value);
         // Yesterday_old_data.push(input_value)
         for (let a = 0; a < Yesterday.length; a++) {
@@ -78,45 +90,44 @@ function ReloadBackup() {
     RenderExisting();
     HeaderDisplay();
 }
+function FunctionsNeedtorunDuringOnload() {
+    ReloadBackup();
+    ShowDone();
+}
 //this function will run during loading
-window.onload = ReloadBackup();
+window.onload = FunctionsNeedtorunDuringOnload();
 
 
 function Add_localStorage(input_value) {
-    let Temp = Yesterday_old_data;
-    Temp.push(input_value)
-    window.localStorage.setItem("TODO", Temp)
+    if (input_value !== undefined || input_value !== null) {
+        let Temp = Yesterday_old_data;
+        Temp.push(input_value)
+        window.localStorage.setItem("TODO", Temp)
+    }
 }
 function AddDoneToLocalStorage(Data) {
-    let Temp_yesterday = window?.localStorage?.getItem("DONE") || []
-    console.log(Temp_yesterday)
-    Temp_yesterday[0] !== undefined ? Temp_yesterday = Temp_yesterday?.split(',') : null
-    Temp_yesterday.push(Data);
-    console.log(Temp_yesterday)
-    window.localStorage.setItem("DONE", Temp_yesterday);
-    location.reload();
+    if (Data !== undefined && Data !== null && Data != "Btn") {
+        let Temp_yesterday = window?.localStorage?.getItem("DONE") || []
+        Temp_yesterday[0] !== undefined ? Temp_yesterday = Temp_yesterday?.split(',') : null
+        Temp_yesterday.push(Data);
+        window.localStorage.setItem("DONE", Temp_yesterday);
+        location.reload();
+    }
 }
 //Attaching events to all the buttons to remove
 document.querySelectorAll('.Btn_done').forEach(function (item) {
     item.addEventListener('click', function () {
         // let ItemToRemove = document.querySelectorAll('.Btn_done')
         let Temp_yesterday = window.localStorage.getItem("TODO") || null
-        Temp_yesterday = Temp_yesterday.split(',');
-        Temp_yesterday = Temp_yesterday.filter(elem => elem !== item.id);
-        window.localStorage.setItem("TODO", Temp_yesterday);
-        AddDoneToLocalStorage(item.id);
-        location.reload();
+        Temp_yesterday = Temp_yesterday?.split(',');
+        Temp_yesterday = Temp_yesterday?.filter(elem => elem !== item.id);
+        if (Temp_yesterday !== undefined) {
+            window.localStorage.setItem("TODO", Temp_yesterday);
+            AddDoneToLocalStorage(item.id);
+            location.reload();
+        }
     });
 });
-// to show the done
-function ShowDone() {
-    let Temp_yesterday = window.localStorage.getItem("DONE") || null
-    Temp_yesterday = Temp_yesterday.split(',');
-    CreateElements(Temp_yesterday, "cont", "Spl");
-    // console.log(Temp_yesterday)
-}
-
-// ShowDone();
 
 //download the done file
 function downloadFile(type) {
